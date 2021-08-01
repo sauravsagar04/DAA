321a3699b08a05bf50f5ee8309c84d752573c3a4
sauravsagar04 <sauravswaraj2001@gmail.com>
2021-08-01 21:16:41 +0530

    Insertion Sort

---
+++ Week03/InsertionSort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int arr[],int size)
+{
+    int i,temp,j;
+    int comp = 0;
+    for(i = 0; i < size; i++)
+    {
+        temp = arr[i];
+        j=i-1;
+        
+        while(arr[j]>temp && j>=0)
+        {
+            arr[j+1] = arr[j];
+            j=j-1;
+            comp+=1;
+        }
+        arr[j+1] = temp;
+    }
+    for(int i = 0; i < size; i++)
+        cout<<arr[i]<<" ";
+    cout<<"\n";
+    cout<<"comparisions = "<<comp<<"\n";
+    cout<<"Swift = "<<comp+size-1<<"\n";  
+
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i = 0; i < n; i++)
+            cin>>arr[i];
+        InsertionSort(arr,n);
+    }
+    return 0;
+}
